c15d6481ce0338a2ae63865dd0482552c3b211ae
youichiokuda <youichiokuda@icloud.com>
2021-01-04 10:18:51 +0900

    second

--- tweetget.py
+++ tweetget.py
@@ -1,6 +1,10 @@
 import tweepy
 import csv
 
+key_word=input('検索したいキーワードを入力してください')
+number=input('取得件数を入力してください')
+file_name=input('結果出力するCSVファイルの名前を入力してください')
+number=int(number)
 
 CONSUMER_KEY = "取得したーキーを入力"
 CONSUMER_SECRET = "取得したーキーを入力"
@@ -12,15 +16,18 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 tweet_data = [] #取得したツイートを格納するリスト
-#q=の後に検索したいキーワード入力
-for tweet in api.search(q="検索したいキーワード入力  exclude:retweets",tweet_mode='extended',count=1500):
+
+for tweet in api.search(q='{}'.format(key_word),tweet_mode='extended',count=number):
     try:
         tweet_data.append([tweet.id, tweet.user.screen_name, tweet.created_at, tweet.full_text.replace('\n',''), tweet.favorite_count, tweet.retweet_count, tweet.user.followers_count, tweet.user.friends_count])
     except Exception as e:
         print(e)
-#出力するCSVファイルの名称を入力
-with open('ファイル名称.csv', 'w',newline='',encoding='utf-8') as f:
+
+with open('{}.csv'.format(file_name), 'w',newline='',encoding='utf-8') as f:
     writer = csv.writer(f, lineterminator='\n')
     writer.writerow(["id","user","created_at","text","fav","RT","follower","follows"])
     writer.writerows(tweet_data)
 pass
+
+print('出力件数:')
+print(len(tweet_data))
